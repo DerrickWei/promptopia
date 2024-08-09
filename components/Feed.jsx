b5141d344ee33811afb392cb968fc71734b087b3
DerrickChen <derrickchen0915@gmail.com>
2024-08-09 11:10:59 -0400

    add search function on feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,12 +3,12 @@
 import { useState, useEffect } from 'react'
 
 import PromptCard from './PromptCard'
+import { set } from 'mongoose'
 
 const PromptCardList = ({ data, handleTagClick }) => {
 	return (
 		<div className="mt_16 prompt_layout">
 			{ data.map( (post) => (
-				console.log(post),
 				<PromptCard 
 					key={post._id}
 					post={post}
@@ -20,11 +20,39 @@ const PromptCardList = ({ data, handleTagClick }) => {
 }
 
 const Feed = () => {
-	const [searchText, setSearchText] = useState('');
 	const [posts, setPosts] = useState([]);
 
+	const [searchText, setSearchText] = useState('');
+	const [searchTimeout, setSearchTimeout] = useState(null);
+	const [searchResults, setSearchResults] = useState([]);
+
+	const filterPrompts = (searchParam) => {
+		const regex = new RegExp(searchParam, "i"); // 'i' flag for case-insensitive search
+
+		return posts.filter(
+			(item) =>
+				regex.test(item.creator.username) ||
+				regex.test(item.tag) ||
+				regex.test(item.prompt)
+		);
+	}
+
 	const handleSearchChange = (e) => {
-	
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		// Debounce search
+		setSearchTimeout(
+			setTimeout(() => {
+				const results = filterPrompts(e.target.value);
+				setSearchResults(results);
+			}, 500)
+		);
+	}
+
+	const handleTagClick = (tag) => {
+		setSearchText(tag);
+		setSearchResults(filterPrompts(tag));
 	}
 
 	useEffect(() => {
@@ -53,10 +81,20 @@ const Feed = () => {
 				/>
 			</form>
 
-			<PromptCardList 
-				data={posts}
-				handleTagClick={ () => {} }
-			/>
+			{searchText && (
+				<PromptCardList 
+					data={searchResults}
+					handleTagClick={handleTagClick}
+				/>
+			)}
+
+			{!searchText && (
+				<PromptCardList 
+					data={posts}
+					handleTagClick={handleTagClick}
+				/>
+			)}
+			
 		</section>
 	)
 }
